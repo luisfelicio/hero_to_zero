5459fe6f398ec02b0f351ac420a72454abbc6c7b
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2021-04-19 00:09:49 +0100

    Update 1.hs

--- Haskell/exercises/1.hs
+++ Haskell/exercises/1.hs
@@ -21,10 +21,13 @@ max3 a b c = if (max2 a c)>b then max2 a c else b
 
 nRaizes :: Float -> Float -> Float -> Int 
 nRaizes a b c | b^2 -4*a*c == 0 = 1
-              | b^2 -4*a*c > 0 = 2                             | otherwise = 0 
+              | b^2 -4*a*c > 0 = 2                             
+              | otherwise = 0 
 
 raizes :: Float -> Float -> Float -> [Float]
-raizes a b c | nRaizes a b c == 0 = []                                      | nRaizes a b c == 1 = [-b/2*a]                                | nRaizes a b c == 2 = [(-b + sqrt((b)^2 -4*a*c))/2*a,(-b - sqrt((b)^2 -4*a*c))/2*a]
+raizes a b c | nRaizes a b c == 0 = []                                      
+             | nRaizes a b c == 1 = [-b/2*a]                                
+             | nRaizes a b c == 2 = [(-b + sqrt((b)^2 -4*a*c))/2*a,(-b - sqrt((b)^2 -4*a*c))/2*a]
 
 
 testehora :: (Int,Int) -> Bool 
@@ -34,7 +37,9 @@ testehora (h,m) | h+m < 0 = False
 
 comphoras :: (Int,Int) -> (Int,Int) -> (Int,Int)
 comphoras (h1,m1) (h2,m2) | h1 > h2 = (h1,m1)
-                              | h1 < h2 = (h2,m2)              | m1 > m2 = (h1,m1)              | otherwise = (h2,m2)
+                              | h1 < h2 = (h2,m2)              
+                              | m1 > m2 = (h1,m1)              
+                              | otherwise = (h2,m2)
 
 convhoras :: (Int,Int) -> Int
 convhoras (hx,mx) = hx*60 + mx 
@@ -46,7 +51,8 @@ difhoras :: (Int,Int) -> (Int,Int) -> Int
 difhoras (ho1,min1) (ho2,min2) = abs(ho1*60 + min1 - (ho2*60 + min2))
 
 adicmin :: Int -> (Int,Int) -> (Int,Int)
-adicmin minx (hoy,miny) | minx + miny > 60 = (hoy + 1, minx + miny - 60)                                      | minx + miny < 60 = (hoy, minx + miny)
+adicmin minx (hoy,miny) | minx + miny > 60 = (hoy + 1, minx + miny - 60)                                      
+                        | minx + miny < 60 = (hoy, minx + miny)
 
 data Hora = H Int Int deriving (Show,Eq)
 
@@ -54,8 +60,10 @@ testehora2 :: Hora -> Bool
 testehora2 (H h m) = h < 24 && m < 60 && h >= 0 && m >= 0
 
 comphoras2 :: Hora -> Hora -> Hora
-comphoras2 hora1@(H h1 m1) hora2@(H h2 m2)| h1 > h2 = hora1
-                              | h1 < h2 = hora2              | m1 > m2 = hora1              | otherwise = hora2
+comphoras2 hora1@(H h1 m1) hora2@(H h2 m2) | h1 > h2 = hora1
+                                           | h1 < h2 = hora2              
+                                           | m1 > m2 = hora1              
+                                           | otherwise = hora2
 
 convhoras2 :: Hora -> Int
 convhoras2 (H hx mx) = hx*60 + mx 
@@ -67,4 +75,5 @@ difhoras2 :: Hora -> Hora -> Int
 difhoras2 (H ho1 min1) (H ho2 min2) = abs(ho1*60 + min1 - (ho2*60 + min2))
 
 adicmin2 :: Int -> Hora -> Hora
-adicmin2 minx (H hoy miny) | minx + miny > 60 = (H (hoy + 1) (minx + miny - 60))                                      | minx + miny < 60 = (H hoy (minx + miny))
+adicmin2 minx (H hoy miny) | minx + miny > 60 = (H (hoy + 1) (minx + miny - 60))                                      
+                           | minx + miny < 60 = (H hoy (minx + miny))
